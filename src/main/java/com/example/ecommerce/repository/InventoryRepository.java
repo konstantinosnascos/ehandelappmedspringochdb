a68af60965daa30901329b37359f8963ce404bd7
kostas900402 <nascoskostas@gmail.com>
2025-12-19 13:34:15 +0100

    inventoryrepo gjord, inte kollat på extra metoder den behöver än

---
+++ src/main/java/com/example/ecommerce/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+}
