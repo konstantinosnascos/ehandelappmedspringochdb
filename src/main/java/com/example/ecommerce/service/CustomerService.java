b6dc29e772059846d1468b57eb82ca4a522e42e7
kostas900402 <nascoskostas@gmail.com>
2025-12-21 11:57:27 +0100

    Klasser helt utan imports och metoder, endast korta kommentarer om ungefär vad som behöver göras i varje klass.

---
+++ src/main/java/com/example/ecommerce/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.service;
+
+public class CustomerService {
+
+    //CRUD kunder
+
+    //Hämta alla kunder
+    //Hämta kund med id
+    //Hämta kund med email
+    //Skapa ny kund
+    //Uppdatera kund
+    //kan behövas fler, men så folk har en ide av vad som behöver göras i klassen
+
+}
---
+++ src/main/java/com/example/ecommerce/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.service;
+
+public class OrderService {
+
+
+    //Hämta alla ordrar
+    //    Hämta order med id
+    //    Hämta ordrar för kund
+    //    Hämta ordrar med viss status
+    //    Skapa order
+    //    Avbryt order
+    //    Sätt status PAID
+
+}
