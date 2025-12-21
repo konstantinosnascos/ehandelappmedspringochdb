1aa81b01d95f706c7cad96c4fb534a1cbfb725f1
kostas900402 <nascoskostas@gmail.com>
2025-12-21 11:08:19 +0100

    extra metoder i customerrepo och inventoryrepo. tester för båda dessa

--- src/main/java/com/example/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/example/ecommerce/repository/CustomerRepository.java
@@ -4,6 +4,11 @@ import com.example.ecommerce.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String mail);
+
+    boolean existsByEmail(String mail);
 }
